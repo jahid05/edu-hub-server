777a60a8f080a082260d69abac69323c07eebbcc
Md Jahid Al Hassan <jahidcse05@gmail.com>
2023-06-14 20:34:24 +0600

    Bug Fix

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
-
-
-
 
 // middleware
 
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o7ebdmy.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -23,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -33,17 +29,24 @@ async function run() {
 
     const courseCollection = client.db("Edu-Hub").collection("coursesDB");
 
-    app.get('/courses', async(req, res) => {
-        const cursor = courseCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/courses", async (req, res) => {
+      const cursor = courseCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    
+    app.get("/courseDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await courseCollection.findOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -51,13 +54,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-    res.send("Edu-Hub is running")
-})
+app.get("/", (req, res) => {
+  res.send("Edu-Hub is running");
+});
 
 app.listen(port, () => {
-    console.log(`Edu Hub server is running: ${port}`);
-})
+  console.log(`Edu Hub server is running: ${port}`);
+});
